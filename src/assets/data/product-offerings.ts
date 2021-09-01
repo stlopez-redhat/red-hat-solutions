2bfac8b858470b6cfbb21e3a81b00e55cdd90d55
PrimeAdvent <43542025+IntuitiveDesigns@users.noreply.github.com>
2021-08-31 21:52:41 -0400

    Various enhancements

--- src/assets/data/product-offerings.ts
+++ src/assets/data/product-offerings.ts
@@ -1,20 +1,20 @@
 import { ProductBenefits } from '../../app/common/model/product-benefits';
 
 export const PRODUCTOFFERINGS: ProductBenefits[] = [
-  { id: 0, title: 'ACH', subtitle: 'ACH Payments',
+  { id: 0, title: 'ACM', subtitle: 'Advanced Cluster Management',
     featureList: [
-      { id: '1', name: 'feature 1', desc: 'feature 1 description' },
-      { id: '1', name: 'feature 2', desc: 'feature 2 description' },
-      { id: '1', name: 'feature 3', desc: 'feature 3 description' },
-      { id: '1', name: 'feature 4', desc: 'feature 4 description' }]
+      { id: '1', name: 'feature 1', desc: 'Multicluster lifecycle management' },
+      { id: '2', name: 'feature 2', desc: 'Policy drive governance, risk, and compliance' },
+      { id: '3', name: 'feature 3', desc: 'Advanced application lifecycle management' },
+      { id: '4', name: 'feature 4', desc: 'Multicluster observability for health and optimization' }]
   },
-  { id: 1, title: 'An Architecture', subtitle: 'That Delivers',
+  { id: 1, title: 'Architecture', subtitle: 'That Delivers',
     featureList: [
-      { id: '2', name: 'feature 1', desc: 'Digital Experiences' },
-      { id: '3', name: 'feature 2', desc: 'Exposed API' },
-      { id: '4', name: 'feature 3', desc: 'Modular (Microservices)' },
-      { id: '5', name: 'feature 4', desc: 'Decoupled (Event Driven)' },
-      { id: '6', name: 'feature 5', desc: 'Progressive Transformation' }
+      { id: '1', name: 'feature 1', desc: 'Digital Experiences' },
+      { id: '2', name: 'feature 2', desc: 'Exposed API' },
+      { id: '3', name: 'feature 3', desc: 'Modular (Microservices)' },
+      { id: '4', name: 'feature 4', desc: 'Decoupled (Event Driven)' },
+      { id: '5', name: 'feature 5', desc: 'Progressive Transformation' }
     ]
   },
   {
@@ -44,25 +44,25 @@ export const PRODUCTOFFERINGS: ProductBenefits[] = [
   {
     id: 5, title: 'Open Banking', subtitle: 'Open Banking',
     featureList: [
-      { id: '3', name: 'feature 1', desc: 'testing' },
-      { id: '4', name: 'feature 2', desc: 'feature 2' }]
+      { id: '1', name: 'feature 1', desc: 'testing' },
+      { id: '2', name: 'feature 2', desc: 'feature 2' }]
   },
   {
     id: 6, title: 'Open Banking', subtitle: 'Open Banking',
     featureList: [
-      { id: '3', name: 'feature 1', desc: 'testing' },
-      { id: '4', name: 'feature 2', desc: 'feature 2' }]
+      { id: '1', name: 'feature 1', desc: 'testing' },
+      { id: '2', name: 'feature 2', desc: 'feature 2' }]
   },
   {
     id: 7, title: 'Open Banking', subtitle: 'Open Banking',
     featureList: [
-      { id: '3', name: 'feature 1', desc: 'testing' },
-      { id: '4', name: 'feature 2', desc: 'feature 2' }]
+      { id: '1', name: 'feature 1', desc: 'testing' },
+      { id: '2', name: 'feature 2', desc: 'feature 2' }]
   },
   {
     id: 8, title: 'Open Banking', subtitle: 'Open Banking',
     featureList: [
-      { id: '3', name: 'feature 1', desc: 'testing' },
-      { id: '4', name: 'feature 2', desc: 'feature 2' }]
+      { id: '1', name: 'feature 1', desc: 'testing' },
+      { id: '2', name: 'feature 2', desc: 'feature 2' }]
   }
 ]
--- src/assets/data/redhat-demo.ts
+++ src/assets/data/redhat-demo.ts
@@ -1,22 +1,22 @@
 import { ProjectDemo } from '../../app/common/model/project-demo';
 
 export const PROJECTDEMO: ProjectDemo[] = [
-  {id: '1', name: 'ACH', title: 'Red Hat ACH Banking',
-    desc: 'At Red Hat we have a passion for what we do! Working with multiple Red Hat products as an ' +
-    'integrated system to streamline your businesses rapidly with high fault tolerance and availability. ' +
-    'Building effective solutions cheaper, faster, better with a modern cloud native architecture.',
+  {id: '1', name: 'ACM', title: 'Red Hat Advanced Cluster Management',
+    desc: 'Red Hat® Advanced Cluster Management for Kubernetes controls clusters and applications from a single console, with built-in security policies. Extend the value of Red Hat OpenShift® by deploying apps, managing multiple clusters, and enforcing policies across multiple clusters at scale.    ',
     redhatTechId: [0, 1, 2, 5],
-    demoImg: '',
+    demoImg: '../../assets/img/acm-features-benefits.jpg',
     productBenefitsId: [0]
   },
   {id: '2', name: 'Real Time Payments', title: 'Red Hat Real Time Payments',
     desc: 'The payments  ecosystem is a  complex  network  of  different,  often  unconnected systems. From cash to cards to mobile in real-time, the payments ecosystem continues to expand. It includes many country-specific electronic payment methods, which are not interoperable.',
     redhatTechId: [3, 4, 5],
-    demoImg: '../../assets/img/target-architecture.jpg',
+    demoImg: '../../assets/img/banking-modernization.jpg',
     productBenefitsId: [1, 2, 3, 4]
   },
   {id: '3', name: 'Open Banking', title: 'Red Hat Support for Open Banking',
-    desc: 'Red Hat brings the future of open banking to reality',
+    desc: 'At Red Hat we have a passion for what we do! Working with multiple Red Hat products as an ' +
+    'integrated system to streamline your businesses rapidly with high fault tolerance and availability. ' +
+    'Building effective solutions cheaper, faster, better with a modern cloud native architecture.',
     redhatTechId: [0, 1, 3, 5],
     demoImg: '',
     productBenefitsId: [6, 7, 8]
